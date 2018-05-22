4aeb5612c590b355e1b4fd1d3860a0ce8f3e9bef
alexander janet <ajt@dev-04.kopaxgroup>
2018-05-23 10:18:10 +1200

    Link

--- packages/ra-ui-bootstrap-styled/src/Link.js
+++ packages/ra-ui-bootstrap-styled/src/Link.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { Link as RRLink } from 'react-router-dom';
-import { withStyles } from 'material-ui/styles';
 
-const styles = theme => ({
-    link: {
-        textDecoration: 'none',
-        color: theme.palette.primary.main,
-    },
-});
-const Link = ({ to, children, className, classes }) => (
-    <RRLink to={to} className={classNames(classes.link, className)}>
+const Link = ({ to, children, className }) => (
+    <RRLink to={to} className={className} style={{ textDecoration: 'none' }}>
         {children}
     </RRLink>
 );
 Link.propTypes = {
     className: PropTypes.string,
-    classes: PropTypes.object,
     children: PropTypes.node,
     to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 };
 
-export default withStyles(styles)(Link);
+export default Link;
